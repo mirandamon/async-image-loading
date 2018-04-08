cd93131953450082f1acc4e2c443d558574eae63
Nathan Miranda <nmiranda4@fordham.edu>
2018-04-08 12:25:43 -0400

    Initial logic to iterate through IMAGE_IDs via splice

--- async-fiesta.js
+++ async-fiesta.js
@@ -1,36 +1,35 @@
+const CHUNK_SIZE = 5
+
 $(() => {
+  let stopped = true
+
   const $bucket = $('.photo-bucket')
   const draw = img => $bucket.append(img)
-
-  let currentIndex = 0
+  const drawCurrentChunk = chunk => chunk.forEach(image => draw(image))
 
   // NOTE: The height and width variables can be changed to fetch different sized images.
   const getImageUrl = id =>
     `https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/cache=expiry:max/rotate=deg:exif/rotate=deg:0/resize=width:30,height:30,fit:crop/output=format:jpg,quality:95/compress/${id}`
 
   const startLoading = () => {
-    // TODO: Implement me.
-    // while (currentIndex <= IMAGE_IDS.length) {
-    for (currentIndex; currentIndex <= IMAGE_IDS.length; currentIndex += 5) {
-      const loadedImages = []
-      const currentBatch = IMAGE_IDS.slice(currentIndex, currentIndex + 5)
-      currentBatch.forEach(imageId => {
-        const currentImage = new Image()
-        currentImage.onload = () => loadedImages.push(currentImage)
-        currentImage.src = getImageUrl(imageId)
-      })
-      console.log(currentIndex)
-      console.log(loadedImages[0])
-      loadedImages.forEach(loadedImage => {
-        console.log('hihihihihi')
-        draw(loadedImage)
-      })
+    stopped = false
+    let chunks = []
+
+    while (IMAGE_IDS.length > 0 && !stopped) {
+      const currentChunk = IMAGE_IDS.splice(0, CHUNK_SIZE)
+      chunks.push(currentChunk)
     }
+
+    // synchronously draw each chunk in the list of chunks
+    return chunks.reduce((chunkQueue, currentChunk) => {
+      drawCurrentChunk(currentChunk)
+    })
   }
 
   const stopLoading = () => {
     // TODO: Implement me.
     console.log('Stop!')
+    stopped = true
   }
 
   $('button.start').on('click', startLoading)
